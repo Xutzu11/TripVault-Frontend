0ff5ce61025a472777026e51d25bf7317bb91a45
Ignat Alex-Matei <ignatalexmatei@yahoo.com>
2025-06-12 15:26:10 +0300

    replace static redirect copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
 import mkcert from 'vite-plugin-mkcert';
-import {viteStaticCopy} from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        mkcert(),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'public/_redirects', // source file
-                    dest: '.', // destination: dist/
-                },
-            ],
-        }),
-    ],
+    plugins: [react(), mkcert()],
     server: {
         port: 3000,
     },
