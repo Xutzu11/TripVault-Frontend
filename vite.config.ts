72f4d2394d3adac7f99c6ef719c3453d2d8a2f8b
Ignat Alex-Matei <ignatalexmatei@yahoo.com>
2025-06-12 14:20:44 +0300

    redirect for dynamic files

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
-import mkcert from 'vite-plugin-mkcert'
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +8,10 @@ export default defineConfig({
     server: {
         port: 3000,
     },
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    },
+    base: '/',
 });
